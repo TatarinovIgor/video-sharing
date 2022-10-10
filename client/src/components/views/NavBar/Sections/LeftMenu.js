6b896b1015f6cb070787a07e66b19f6374c668c3
IgorTatarinov <ig.tatarinov@gmail.com>
2022-10-10 11:42:43 +0300

    Video upload form added

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -9,6 +9,11 @@ function LeftMenu(props) {
     <Menu.Item key="mail">
       <a href="/">Home</a>
     </Menu.Item>
+
+    <Menu.Item key="upload">
+      <a href="/upload">Upload</a>
+    </Menu.Item>
+
     <SubMenu title={<span>Blogs</span>}>
       <MenuItemGroup title="Item 1">
         <Menu.Item key="setting:1">Option 1</Menu.Item>
